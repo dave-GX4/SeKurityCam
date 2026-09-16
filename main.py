e557b632e38e19f830559b562182625822989e6a
dsave-GX4 <angoga344@gmail.com>
2026-09-16 14:21:13 -0600

    Detección de camara con python basico

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("No se pudo abrir la cámara")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("No se recibió el fotograma")
+        break
+
+    cv2.imshow("Cámara en vivo", frame)
+
+    # Presiona 'q' para salir
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
